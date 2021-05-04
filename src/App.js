45f3810a79c7230948d8722e0c4005f42ae523b1
MiriamMVentura <miriamartv@gmail.com>
2021-05-03 22:50:00 -0500

    feat(react settings): add react, editorConfig, Eslint and react router

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './App.css';
+
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+
+function App() {
+  return (
+  <Router>
+    <div className="App">
+      <Switch>
+        <Route path = '/' exact>
+          <h1>Hola soy el home</h1>
+        </Route>
+        <Route path = '/login'>
+          <h1>Hola soy el login</h1>
+        </Route>
+        <Route path = '/order'>
+          <h1>Hola soy para las ordenes</h1>
+        </Route>
+        <Route path = '/kitchen'>
+          <h1>Hola soy el jefe de cocina</h1>
+        </Route>
+        <Route path = '/admin'>
+          <h1>Hola soy el administrador</h1>
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+  );
+}
+
+export default App;
